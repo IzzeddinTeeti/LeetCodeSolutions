485a77578a1a996086e834f2b0574735f1c97ea8
IzzeddinTeeti <ezz.msa@hotmail.com>
2023-03-15 18:29:38 +0000

    Time: 185 ms (5.29%), Space: 14 MB (74.08%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -9,3 +9,9 @@ class Solution:
 
             if counter == k:
                 return num
+            
+            
+            
+# https://leetcode.com/problems/kth-missing-positive-number/discuss/3262163/C%2B%2B-Java-Python3-1-line-O(logn)
+    
+# https://leetcode.com/problems/kth-missing-positive-number/discuss/3262072/Python3-oror-44-ms-faster-than-96.22-of-Python3
