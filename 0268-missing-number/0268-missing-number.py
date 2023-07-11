b0ee26d28f2ea479248530f214a950c84a3afd97
IzzeddinTeeti <ezz.msa@hotmail.com>
2023-07-11 18:38:13 +0100

    Time: 153 ms (36.55%), Space: 17.6 MB (82.05%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        upper_limit = len(nums)
-        exist = {}
-        
-        for num in range(upper_limit +1):
-            if num not in nums:
-                return num
+        n = len(nums)
+        total_sum = (n * (n + 1)) // 2  # Sum of numbers from 0 to n
+
+        array_sum = sum(nums)  # Sum of elements in the array
+
+        return total_sum - array_sum
         
