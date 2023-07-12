a31e515b00629cf859700ddea9c8f78ff37d5c27
IzzeddinTeeti <ezz.msa@hotmail.com>
2023-07-12 12:44:29 +0100

    Time: 109 ms (31.02%), Space: 16.5 MB (45.83%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -1,18 +1,16 @@
-import numpy as np
 class Solution:
     def surfaceArea(self, grid: List[List[int]]) -> int:
         
-        grid = np.array(grid)
         n = len(grid)
         area = 0
         
         for row in range(n):
             for col in range(n):
-                if grid[row, col]:
-                    area += 4 * grid[row, col] + 2
+                if grid[row][col]:
+                    area += 4 * grid[row][col] + 2
                 if row+1 < n:
-                    area -= 2 * min(grid[row, col], grid[row+1, col])
+                    area -= 2 * min(grid[row][col], grid[row+1][col])
                 if col+1 < n:
-                    area -= 2 * min(grid[row, col], grid[row, col+1])     
+                    area -= 2 * min(grid[row][col], grid[row][col+1])     
         
         return area
