b7bd4ab471bd54d7f005b31f0b87ed59f4a42352
IzzeddinTeeti <ezz.msa@hotmail.com>
2023-07-12 12:31:14 +0100

    Time: 109 ms (31.02%), Space: 16.3 MB (97.69%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -2,20 +2,17 @@ class Solution:
     def surfaceArea(self, grid: List[List[int]]) -> int:
         
         n = len(grid)
-        top_bot = sum([sum([2 if i>0 else 0 for i in row]) for row in grid])
-        
-        side = 0
+        area = 0
         
         for row in range(n):
             for col in range(n):
                 if grid[row][col]:
-                    side += 4 * grid[row][col] + 2
+                    area += 4 * grid[row][col] + 2
                 if row+1 < n:
-                    side -= 2 * min(grid[row][col], grid[row+1][col])
+                    area -= 2 * min(grid[row][col], grid[row+1][col])
                 if col+1 < n:
-                    side -= 2 * min(grid[row][col], grid[row][col+1])
+                    area -= 2 * min(grid[row][col], grid[row][col+1])
                 
                 
         
-        res = side + top_bot        
-        return side
+        return area
