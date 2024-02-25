8e07420ad1ded6b56e5f017eece7addda43068fb
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-02-25 23:00:42 +0000

    Time: 27 ms (97.95%), Space: 16.5 MB (99.17%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,21 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        # s = set(s)
-#         s = list(s)
-#         t = list(t)
-        
         tot_s = sum([ord(char) for char in s])
         tot_t = sum([ord(char) for char in t])
         
         diff = tot_t - tot_s
         
         return chr(diff)
-        
-#         for char in s:
-#             if char in t:
-#                 del t[char]
-        
-#         return t[0]
-            
-        
+    
