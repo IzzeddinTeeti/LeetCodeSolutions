aaee7ec55382abb5457ab450ed74b194d79a2472
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-02-25 20:53:32 +0000

    Time: 111 ms (64.09%), Space: 19.1 MB (83.32%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
         
-        n = len(nums)
-        passed = {}
+#         n = len(nums)
+#         passed = {}
         
-        for i in nums:
-            if i in passed.keys():
-                del passed[i]
+#         for i in nums:
+#             if i in passed.keys():
+#                 del passed[i]
                 
-            else:
-                passed[i] = 1
+#             else:
+#                 passed[i] = 1
                 
         
-        answer = list(passed.keys())
-        return answer[0]
+#         answer = list(passed.keys())
+#         return answer[0]
             
