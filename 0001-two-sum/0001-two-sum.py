56e743555bd41940ca0e3a13918330b211961aac
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-02-25 20:36:53 +0000

    Time: 60 ms (66.37%), Space: 17.7 MB (64.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        # nums_set = set(nums)
+        passed = {}
+        
         for ind, num in enumerate(nums):
-            nums_set = set(nums[:ind] + nums[ind+1:])
-            if target-num in nums_set and ind != nums.index(target-num):
-                return [ind, nums.index(target-num)]
+            
+            if target- num in passed.keys():
+                return [ind, passed[target-num]]
+            else:
+                passed[num] = ind
