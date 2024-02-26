3f4b682fb1a9cced1c73934d7a11e69d59f12c0f
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-02-26 21:36:52 +0000

    Time: 69 ms (99.64%), Space: 19.4 MB (90.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        visited = {}
+        
+        for word in strs:
+            
+            ordered = ''.join(sorted(word))
+            
+            if ordered in visited.keys():
+                visited[ordered].append(word)
+            else:
+                visited[ordered] = [word]
+            
+        return list(visited.values())
