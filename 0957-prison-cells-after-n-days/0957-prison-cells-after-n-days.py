d78fbe44aa72a05d6fee004ffb04c1a58cbfa3e3
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-03-15 08:15:58 +0000

    Time: 40 ms (62.97%), Space: 16.6 MB (40.09%) - LeetHub

---
+++ 0957-prison-cells-after-n-days/0957-prison-cells-after-n-days.py
@@ -0,0 +1,20 @@
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+        
+        n = max((n-1)%14 + 1, 1)
+        total = len(cells)
+        
+        for day in range(n):
+            new_cells = [0] * total
+            
+            for cell in range(1,7):
+                new_cells[cell] = int(cells[cell-1]==cells[cell+1])
+            
+            cells = new_cells
+            
+        return cells
+                    
+        
+                
+                    
+        
