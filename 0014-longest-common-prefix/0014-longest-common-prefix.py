93e42cbe1de3235586ddd40358851c510c6b9be0
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-12-01 15:50:45 +0300

    Time: 0 ms (100%), Space: 17.3 MB (5.28%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,23 +1,39 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        lengths = [len(word) for word in strs]
-        print(lengths)
+        # lengths = [len(word) for word in strs]
+        # print(lengths)
 
-        min_length = min(lengths)
-        shortest_str_idx = lengths.index(min_length)
-        shortest_str = strs[shortest_str_idx]
-        # print(min_length, shortest_str)
+        # min_length = min(lengths)
+        # shortest_str_idx = lengths.index(min_length)
+        # shortest_str = strs[shortest_str_idx]
+        # # print(min_length, shortest_str)
 
+        # ans = ''
+        # chars = ''
+
+        # for char in shortest_str:
+        #     chars += char
+
+        #     x = [word.startswith(chars) for word in strs]
+        #     if all(x):
+        #         ans = chars
+        #     # print(chars, x, ans)
+        # return ans
+
+        strs.sort()
+
+        first = strs[0]
+        last = strs[-1]
+        i = 0
         ans = ''
-        chars = ''
 
-        for char in shortest_str:
-            chars += char
-
-            x = [word.startswith(chars) for word in strs]
-            if all(x):
-                ans = chars
-            # print(chars, x, ans)
+        while i < len(first):
+            if first[i] == last[i]:
+                ans += first[i]
+                i += 1
+            else:
+                break
         return ans
+
         
