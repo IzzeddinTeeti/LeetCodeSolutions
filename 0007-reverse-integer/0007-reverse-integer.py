2cf18ac2f3ce7c8df39387fd83f28a2050d74f97
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-12-01 12:45:22 +0300

    Time: 29 ms (91.14%), Space: 17.2 MB (5.1%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,25 +1,28 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        INT_MIN, INT_MAX = -2**31, 2**31 - 1
-
+        int_min = -2**31
+        int_max = 2**31 - 1
+        
         negative = x < 0
         x = abs(x)
-
-        # Reverse the digits of the absolute value
         reversed_x = 0
+
         while x != 0:
-            digit = x % 10  # Get the last digit
-            reversed_x = reversed_x * 10 + digit
-            x //= 10  # Remove the last digit
-
-        # Restore the negative sign if needed
+            number = x % 10
+            reversed_x = reversed_x * 10 + number
+            x = x // 10
+        
         if negative:
-            reversed_x = -reversed_x
+            reversed_x *= -1
 
-        if reversed_x < INT_MIN or reversed_x > INT_MAX:
+
+        if reversed_x < int_min or reversed_x > int_max:
             return 0
-
+        
         return reversed_x
+        
+
+
 
         
         
