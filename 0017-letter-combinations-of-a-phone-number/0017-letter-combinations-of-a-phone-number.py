71228cfb4c139cb6763c2556870822746c9a4c9b
IzzeddinTeeti <ezz.msa@hotmail.com>
2024-12-01 22:47:19 +0300

    Time: 0 ms (100%), Space: 17.3 MB (5.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
-        if digits == '':
+        
+        if digits =='':
             return []
 
-        phone = {'2': ['a','b','c'],
-        '3': ['d','e','f'],
-        '4': ['g','h','i'],
-        '5': ['j','k','l'],
-        '6': ['m','n','o'],
-        '7': ['p','q','r','s'],
-        '8': ['t','u','v'],
-        '9': ['w','x','y','z']}
+        phone = {'2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'}   
 
         results = ['']
-        total = []
-        n = len(digits)
 
-        for ind, digit in enumerate(digits):
+        for digit in digits:
             letters = phone[digit]
-            # print(digit, letters)
             results = [prefix+letter for prefix in results for letter in letters]
-        
-        return results                  
+
+        return results             
